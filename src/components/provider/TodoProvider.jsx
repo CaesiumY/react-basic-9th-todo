424d84a3363a72d05a7257dd6996343c50770162
caesiumy <dbs2636@gmail.com>
2025-02-10 12:08:31 +0900

    feat: TodoProvider에서 샘플 할 일 목록 추가 및 상태 관리 기능 구현

--- src/components/provider/TodoProvider.jsx
+++ src/components/provider/TodoProvider.jsx
@@ -1,7 +1,56 @@
+import { useState } from "react";
 import { TodoContext } from "../../context/TodoContext";
 
+const SAMPLE_TODOS = [
+  { id: 1, text: "Buy milk", completed: false },
+  { id: 2, text: "Clean the house", completed: false },
+  { id: 3, text: "Go for a run", completed: false },
+  { id: 4, text: "Finish homework", completed: false },
+  { id: 5, text: "Call mom", completed: false },
+  { id: 6, text: "Buy groceries", completed: false },
+  { id: 7, text: "Walk the dog", completed: false },
+  { id: 8, text: "Read a book", completed: false },
+  { id: 9, text: "Do laundry", completed: false },
+  { id: 10, text: "Write code", completed: false },
+];
+
 const TodoProvider = ({ children }) => {
-  return <TodoContext.Provider value={{}}>{children}</TodoContext.Provider>;
+  const [todos, setTodos] = useState(SAMPLE_TODOS);
+
+  const addTodos = (text) => {
+    setTodos([{ id: crypto.randomUUID(), text, completed: false }, ...todos]);
+  };
+
+  const toggleTodoCompleted = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id
+          ? {
+              ...todo,
+              completed: !todo.completed,
+            }
+          : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    // todo.id가 내가 찾는 id와 같지 않을 때 true를 반환하여 그대로 남겨둠
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <TodoContext.Provider
+      value={{
+        todos,
+        addTodos,
+        toggleTodoCompleted,
+        deleteTodo,
+      }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
 };
 
 export default TodoProvider;
--- src/components/todo/TodoContainer.jsx
+++ src/components/todo/TodoContainer.jsx
@@ -1,58 +1,14 @@
-import { useState } from "react";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 import TodoDashboard from "./TodoDashboard";
 import styled from "styled-components";
 
-const SAMPLE_TODOS = [
-  { id: 1, text: "Buy milk", completed: false },
-  { id: 2, text: "Clean the house", completed: false },
-  { id: 3, text: "Go for a run", completed: false },
-  { id: 4, text: "Finish homework", completed: false },
-  { id: 5, text: "Call mom", completed: false },
-  { id: 6, text: "Buy groceries", completed: false },
-  { id: 7, text: "Walk the dog", completed: false },
-  { id: 8, text: "Read a book", completed: false },
-  { id: 9, text: "Do laundry", completed: false },
-  { id: 10, text: "Write code", completed: false },
-];
-
 const TodoContainer = () => {
-  const [todos, setTodos] = useState(SAMPLE_TODOS);
-
-  const addTodos = (text) => {
-    setTodos([{ id: crypto.randomUUID(), text, completed: false }, ...todos]);
-  };
-
-  const toggleTodoCompleted = (id) => {
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) =>
-        todo.id === id
-          ? {
-              ...todo,
-              completed: !todo.completed,
-            }
-          : todo
-      )
-    );
-  };
-
-  const deleteTodo = (id) => {
-    // todo.id가 내가 찾는 id와 같지 않을 때 true를 반환하여 그대로 남겨둠
-    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
-  };
-
   return (
     <TodoContainerWrapper>
       <TodoDashboard />
-
-      <TodoList
-        todos={todos}
-        toggleTodoCompleted={toggleTodoCompleted}
-        deleteTodo={deleteTodo}
-      />
-
-      <TodoForm addTodos={addTodos} />
+      <TodoList />
+      <TodoForm />
     </TodoContainerWrapper>
   );
 };
