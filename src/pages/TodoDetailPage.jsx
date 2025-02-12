81e8bdb7eb42d06b6e9c15c7d7343da80033161e
caesiumy <dbs2636@gmail.com>
2025-02-12 12:55:42 +0900

    feat: TodoProvider 추가 및 샘플 할 일 데이터 상수화, TodoDetailPage에서 할 일 상세 정보 표시

--- src/pages/TodoDetailPage.jsx
+++ src/pages/TodoDetailPage.jsx
@@ -1,9 +1,25 @@
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
+import TodoItem, { ActionButton } from "../components/todo/TodoItem";
+import { SAMPLE_TODOS } from "../constants/sample-todos";
 
 const TodoDetailPage = () => {
   const { id } = useParams();
 
-  return <div>TodoDetailPage ID: {id}</div>;
+  const targetTodoItem = SAMPLE_TODOS.find((todo) => todo.id === id);
+
+  return (
+    <div>
+      <TodoItem
+        id={targetTodoItem.id}
+        text={targetTodoItem.text}
+        completed={targetTodoItem.completed}
+      />
+
+      <ActionButton $bgColor="#242424">
+        <Link to="/">돌아가기</Link>
+      </ActionButton>
+    </div>
+  );
 };
 
 export default TodoDetailPage;
