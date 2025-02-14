72db94fb686219a1a9c25ae744501800b3f2dfb4
caesiumy <dbs2636@gmail.com>
2025-02-14 12:40:31 +0900

    refactor: TodoProvider에서 필터링 기능 추가 및 TodoDashboard와 TodoList에서 사용

--- src/components/provider/TodoProvider.jsx
+++ src/components/provider/TodoProvider.jsx
@@ -27,6 +27,18 @@ const TodoProvider = ({ children }) => {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
   };
 
+  const getFilteredTodos = (selectedFilter) => {
+    if (selectedFilter === "completed") {
+      return todos.filter((todo) => todo.completed);
+    }
+
+    if (selectedFilter === "pending") {
+      return todos.filter((todo) => !todo.completed);
+    }
+
+    return todos;
+  };
+
   return (
     <TodoContext.Provider
       value={{
@@ -34,6 +46,7 @@ const TodoProvider = ({ children }) => {
         addTodos,
         toggleTodoCompleted,
         deleteTodo,
+        getFilteredTodos,
       }}
     >
       {children}
