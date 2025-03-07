b5b0bd02b8cf5e1adbf3b2d5b23ba6d7435052fe
caesiumy <dbs2636@gmail.com>
2025-03-07 12:48:31 +0900

    feat: 기본 Todo 앱 구조 설정 및 상세 페이지 추가

---
+++ app/detail/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+const DetailPage = async () => {
+  const data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const todo = await data.json();
+
+  return (
+    <div>
+      DetailPage
+      <h1>{todo.title}</h1>
+      <Link href="/">Home Page</Link>
+    </div>
+  );
+};
+
+export default DetailPage;
