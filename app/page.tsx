15ce860a32cb426daf0678a48706529d971b7818
caesiumy <dbs2636@gmail.com>
2025-03-21 13:08:44 +0900

    feat: Supabase 클라이언트를 사용하여 Todo API 함수에 의존성 주입 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import { getTodos } from "@/api/todo-api";
 import TodoFilterSwitch from "@/components/todo/TodoFilterSwitch";
 import TodoForm from "@/components/todo/TodoForm";
 import TodoList from "@/components/todo/TodoList";
+import { createClient } from "@/utils/supabase/server";
 import {
   dehydrate,
   HydrationBoundary,
@@ -10,16 +12,17 @@ import React from "react";
 
 const HomePage = async () => {
   const queryClient = new QueryClient();
+  const supabaseClient = await createClient();
 
-  // await queryClient.prefetchQuery({
-  //   queryKey: ["todos", "all"],
-  //   queryFn: () => getTodos(),
-  // });
+  await queryClient.prefetchQuery({
+    queryKey: ["todos", "all"],
+    queryFn: () => getTodos(supabaseClient),
+  });
 
-  // await queryClient.prefetchQuery({
-  //   queryKey: ["todos", "completed"],
-  //   queryFn: () => getTodos("completed"),
-  // });
+  await queryClient.prefetchQuery({
+    queryKey: ["todos", "completed"],
+    queryFn: () => getTodos(supabaseClient, "completed"),
+  });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
