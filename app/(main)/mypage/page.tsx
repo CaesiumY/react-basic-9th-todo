48c5b004dc3633b7c468848c4bb9cd90a1752799
caesiumy <dbs2636@gmail.com>
2025-03-26 12:34:42 +0900

    feat: MyPageCard 컴포넌트를 Suspense로 감싸 로딩 상태 처리 추가

--- app/(main)/mypage/page.tsx
+++ app/(main)/mypage/page.tsx
@@ -1,12 +1,14 @@
 import { MyPageCard } from "@/components/auth/MyPageCard";
 import UserTodoList from "@/components/todo/UserTodoList";
-import React from "react";
+import React, { Suspense } from "react";
 
 const MyPage = () => {
   return (
     <section className="flex min-h-svh w-full justify-center p-6 md:p-10">
       <div className="w-full space-y-8">
-        <MyPageCard />
+        <Suspense>
+          <MyPageCard />
+        </Suspense>
         <UserTodoList />
       </div>
     </section>
