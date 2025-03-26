6f567a7c130402b1df1193870568e7a6c8ec6745
caesiumy <dbs2636@gmail.com>
2025-03-26 12:38:01 +0900

    feat: TodoList 컴포넌트에 애니메이션 효과 추가

--- components/todo/TodoList.tsx
+++ components/todo/TodoList.tsx
@@ -4,13 +4,15 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import { useTodosQuery } from "@/query/useTodoQuery";
 import { useTodoFilterStore } from "@/store/useTodoFilterStore";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const TodoList = () => {
   const { filter } = useTodoFilterStore();
   const { data: todos } = useTodosQuery(filter);
+  const [parent] = useAutoAnimate();
 
   return (
-    <ul className="space-y-2">
+    <ul ref={parent} className="space-y-2">
       {todos?.map((todo) => (
         <li key={todo.id}>
           <TodoItem todo={todo} />
--- components/todo/UserTodoList.tsx
+++ components/todo/UserTodoList.tsx
@@ -3,17 +3,19 @@
 import { useUserTodosQuery } from "@/query/useTodoQuery";
 import { useTodoFilterStore } from "@/store/useTodoFilterStore";
 import TodoItem from "./TodoItem";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const UserTodoList = () => {
   const { filter } = useTodoFilterStore();
   const { data: todos } = useUserTodosQuery(filter);
+  const [parent] = useAutoAnimate();
 
   if (!todos?.length) {
     return <p>No todos found.</p>;
   }
 
   return (
-    <ul className="space-y-2">
+    <ul ref={parent} className="space-y-2">
       {todos?.map((todo) => (
         <li key={todo.id}>
           <TodoItem todo={todo} />
